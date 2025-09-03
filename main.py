2269e2815b8d1b5c0fee34250697e99a267792bd
aubarrio <aubarrios142@gmail.com>
2025-09-03 04:05:16 -0700

    initial tutorial

--- main.py
+++ main.py
@@ -28,3 +28,21 @@ def update_item(item_id: int, item: Item):
         "item_name": item.name,
         "item_id": item_id
     }
+
+# http://127.0.0.1:8000/users/100/items/abcd123?q=idk&short=True
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(user_id: int, item_id: str, q: str | None = None, short: bool = False):
+    item = {
+        "item_id": item_id,
+        "owner_id": user_id
+    }
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {
+                "description": "This is an amazing item that has a long description"
+            }
+        )
+    return item
